783ff66dcb53d04445e72d0e7182850dfbf6fe33
Shivan Sivakumarnan <51132467+ShivanS93@users.noreply.github.com>
2022-01-26 08:54:53 +1300

    fixing base.py typo

--- config/settings/base.py
+++ config/settings/base.py
@@ -40,7 +40,7 @@ AUTH_USER_MODEL = "users.CustomUser"
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware"
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
